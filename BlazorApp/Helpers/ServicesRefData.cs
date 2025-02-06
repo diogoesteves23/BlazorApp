de53593db09d8474b45c279a6703d334e3b8f5c0
diogoesteves23 <92594813+diogoesteves23@users.noreply.github.com>
2025-02-06 15:44:03 +0000

    Update ServicesRefData.cs

--- BlazorApp/Helpers/ServicesRefData.cs
+++ BlazorApp/Helpers/ServicesRefData.cs
@@ -22,55 +22,55 @@ namespace BlazorApp.Helpers
             {
                 "D", new Dictionary<int, string>()
                 { 
-                    {1, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/api/trading/clearaccount/markaccountwithbadaccount?{0}" },
-                    {2, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/getfilesindiretory?prefixPathOrigin={0}&folderNameOrigin={1}&{2}" },
-                    {3, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/copyfilesindiretory" },
-                    {4, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/movefilesindiretory" },
-                    {5, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/getjobinfo?jobId={0}" },
-                    {6, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullerdumpjobexecution?{0}" },
-                    {7, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullerresetbusinessdate" },
-                    {8, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullersetjobschanges" },
-                    {9, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullerinsertjobexecution" },
-                    {10, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullerchangejobexecution" },
-                    {11, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullertriggerrunexecution" },
-                    {12, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/getisinnotfound?{0}" },
-                    {13, "https://dotnet-service-trading-internal-dev-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/insertdummyisin" }
+                    {1, "https://hostname/api/trading/clearaccount/markaccountwithbadaccount?{0}" },
+                    {2, "https://hostname/job/trading/auxiliar/getfilesindiretory?prefixPathOrigin={0}&folderNameOrigin={1}&{2}" },
+                    {3, "https://hostname/job/trading/auxiliar/copyfilesindiretory" },
+                    {4, "https://hostname/job/trading/auxiliar/movefilesindiretory" },
+                    {5, "https://hostname/job/trading/auxiliar/getjobinfo?jobId={0}" },
+                    {6, "https://hostname/job/trading/auxiliar/schedullerdumpjobexecution?{0}" },
+                    {7, "https://hostname/job/trading/auxiliar/schedullerresetbusinessdate" },
+                    {8, "https://hostname/job/trading/auxiliar/schedullersetjobschanges" },
+                    {9, "https://hostname/job/trading/auxiliar/schedullerinsertjobexecution" },
+                    {10, "https://hostname/job/trading/auxiliar/schedullerchangejobexecution" },
+                    {11, "https://hostname/job/trading/auxiliar/schedullertriggerrunexecution" },
+                    {12, "https://hostname/job/trading/auxiliar/getisinnotfound?{0}" },
+                    {13, "https://hostname/job/trading/auxiliar/insertdummyisin" }
                 }
             },
             {
                 "Q", new Dictionary<int, string>()
                 {
-                    {1, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/api/trading/clearaccount/markaccountwithbadaccount?{0}" },
-                    {2, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/getfilesindiretory?prefixPathOrigin={0}&folderNameOrigin={1}&{2}" },
-                    {3, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/copyfilesindiretory" },
-                    {4, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/movefilesindiretory" },
-                    {5, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/getjobinfo?jobId={0}" },
-                    {6, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullerdumpjobexecution?{0}" },
-                    {7, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullerresetbusinessdate" },
-                    {8, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullersetjobschanges" },
-                    {9, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullerinsertjobexecution" },
-                    {10, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullerchangejobexecution" },
-                    {11, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/schedullertriggerrunexecution" },
-                    {12, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/getisinnotfound?{0}" },
-                    {13, "https://dotnet-service-trading-internal-qua-wealthmanagement.apps.aro03.bdso.tech/job/trading/auxiliar/insertdummyisin" }
+                    {1, "https://hostname/api/trading/clearaccount/markaccountwithbadaccount?{0}" },
+                    {2, "https://hostname/job/trading/auxiliar/getfilesindiretory?prefixPathOrigin={0}&folderNameOrigin={1}&{2}" },
+                    {3, "https://hostname/job/trading/auxiliar/copyfilesindiretory" },
+                    {4, "https://hostname/job/trading/auxiliar/movefilesindiretory" },
+                    {5, "https://hostname/job/trading/auxiliar/getjobinfo?jobId={0}" },
+                    {6, "https://hostname/job/trading/auxiliar/schedullerdumpjobexecution?{0}" },
+                    {7, "https://hostname/job/trading/auxiliar/schedullerresetbusinessdate" },
+                    {8, "https://hostname/job/trading/auxiliar/schedullersetjobschanges" },
+                    {9, "https://hostname/job/trading/auxiliar/schedullerinsertjobexecution" },
+                    {10, "https://hostname/job/trading/auxiliar/schedullerchangejobexecution" },
+                    {11, "https://hostname/job/trading/auxiliar/schedullertriggerrunexecution" },
+                    {12, "https://hostname/job/trading/auxiliar/getisinnotfound?{0}" },
+                    {13, "https://hostname/job/trading/auxiliar/insertdummyisin" }
                 }
             },
             {
                 "P", new Dictionary<int, string>()
                 {
-                    {1, "https://aro-prd.apps.aro04.bdso.tech/api/trading/clearaccount/markaccountwithbadaccount?{0}" },
-                    {2, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/getfilesindiretory?prefixPathOrigin={0}&folderNameOrigin={1}&{2}" },
-                    {3, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/copyfilesindiretory" },
-                    {4, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/movefilesindiretory" },
-                    {5, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/getjobinfo?jobId={0}" },
-                    {6, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/schedullerdumpjobexecution?{0}" },
-                    {7, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/schedullerresetbusinessdate" },
-                    {8, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/schedullersetjobschanges" },
-                    {9, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/schedullerinsertjobexecution" },
-                    {10, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/schedullerchangejobexecution" },
-                    {11, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/schedullertriggerrunexecution" },
-                    {12, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/getisinnotfound?{0}" },
-                    {13, "https://aro-prd.apps.aro04.bdso.tech/job/trading/auxiliar/insertdummyisin" }
+                    {1, "https://hostname/api/trading/clearaccount/markaccountwithbadaccount?{0}" },
+                    {2, "https://hostname/job/trading/auxiliar/getfilesindiretory?prefixPathOrigin={0}&folderNameOrigin={1}&{2}" },
+                    {3, "https://hostname/job/trading/auxiliar/copyfilesindiretory" },
+                    {4, "https://hostname/job/trading/auxiliar/movefilesindiretory" },
+                    {5, "https://hostname/job/trading/auxiliar/getjobinfo?jobId={0}" },
+                    {6, "https://hostname/job/trading/auxiliar/schedullerdumpjobexecution?{0}" },
+                    {7, "https://hostname/job/trading/auxiliar/schedullerresetbusinessdate" },
+                    {8, "https://hostname/job/trading/auxiliar/schedullersetjobschanges" },
+                    {9, "https://hostname/job/trading/auxiliar/schedullerinsertjobexecution" },
+                    {10, "https://hostname/job/trading/auxiliar/schedullerchangejobexecution" },
+                    {11, "https://hostname/job/trading/auxiliar/schedullertriggerrunexecution" },
+                    {12, "https://hostname/job/trading/auxiliar/getisinnotfound?{0}" },
+                    {13, "https://hostname/job/trading/auxiliar/insertdummyisin" }
                 }
             }
         };

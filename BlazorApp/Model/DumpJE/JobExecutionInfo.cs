88f3ea078c32a9e6b97f6940286f68f042471aa3
diogoesteves23 <92594813+diogoesteves23@users.noreply.github.com>
2025-02-06 15:40:35 +0000

    Add files via upload

---
+++ BlazorApp/Model/DumpJE/JobExecutionInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorApp.Model.DumpJE
+{
+    public class JobExecutionInfo()
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("jobId")]
+        public long JobId { get; set; }
+
+        [JsonPropertyName("schedulledDate")]
+        public DateTime SchedulledDate { get; set; }
+
+        [JsonPropertyName("startDate")]
+        public DateTime? StartDate { get; set; }
+
+        [JsonPropertyName("endDate")]
+        public DateTime? EndDate { get; set; }
+
+        [JsonPropertyName("businessDate")]
+        public DateTime BusinessDate { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("jobStatus")]
+        public int JobStatus { get; set; }
+
+        [JsonPropertyName("lastExecution")]
+        public DateTime? LastExecution { get; set; }
+
+        [JsonPropertyName("jobSequence")]
+        public long JobSequence { get; set; }
+    }
+}
---
+++ BlazorApp/Model/Services/Service.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Model.Services
+{
+    public class Service
+    {
+        public string PageName { get; set; }
+
+        public string PageCode { get; set; }
+    }
+}

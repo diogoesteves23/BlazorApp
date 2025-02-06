4b0a99b8f9e17bdac2aa728f219a9ca5a7a0245d
diogoesteves23 <92594813+diogoesteves23@users.noreply.github.com>
2025-02-06 15:29:29 +0000

    Add files via upload

---
+++ Model/DumpJE/DumpExecutionOut.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorApp.Model.DumpJE
+{
+    public class DumpExecutionOut()
+    {
+        [JsonPropertyName("data")]
+        public List<JobExecutionInfo> JobExecutionList { get; set; } = new();
+    }
+}
